f0cb3acb862a558aca8ffa3533fed60cd2ebbbd8
vcarlos97 <vcarlos139@gmail.com>
2020-10-23 21:29:58 +0200

    comentarios

--- src/routes/subject.routes.ts
+++ src/routes/subject.routes.ts
@@ -1,6 +1,12 @@
 import {Router} from 'express';
 import subjectController from "../controllers/subject.controller";
 
+/*
+- listado de asignaturas (CU_1)
+- añadir alumno en una asignatura (CU_2)
+- ver detalle de una asignatura (CU_3)
+- ver detalle de un alumno dentro de una asignatura (CU_4)*/
+
 const router = Router();
 router.get('/todo', subjectController.getSubjects);
 
