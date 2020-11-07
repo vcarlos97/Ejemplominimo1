d892672ff492cf9d36390e3b112610df5dd8d954
vcarlos97 <vcarlos139@gmail.com>
2020-11-07 18:16:27 +0100

    tocando mas aun

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { SubjectsComponent } from './subjects/subjects.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { SubjectFormComponent } from './subject-form/subject-form.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/subjects'},
   { path: 'subjects', component: SubjectsComponent},
+  { path: 'newSubject', component: SubjectFormComponent},
+  { path: 'newStudent', component: StudentFormComponent}
 ];
 
 @NgModule({
